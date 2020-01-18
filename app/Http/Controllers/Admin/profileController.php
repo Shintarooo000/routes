3b34fccc467bd9d0be2211a6f6e2e135a660d90d
Shintarooo000 <kataoka240407@gmail.com>
2020-01-18 06:57:38 +0000

    PHP/Laravel 08

---
+++ app/Http/Controllers/Admin/profileController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class profileController extends Controller
+{
+    //ACtion追加
+    public function add(){
+         return view('admin.profile.create');
+    }
+    public function create(){
+        return redirect('/admin/profile/create');
+    }
+    public function edit(){
+        return view('/admin.plofile.edit');
+    }
+    public function update(){
+        return redirect('admin/profile/edit');
+    }
+}
+?>
